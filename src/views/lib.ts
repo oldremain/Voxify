5651041f7a1c9554170aeebb4bc2dcc0ccfe18a5
Олег Зайцев <30oleg.zv94@gmail.com>
2025-06-08 17:29:40 +0300

    fix: import lodash omit

--- src/views/lib.ts
+++ src/views/lib.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
+import { omit } from 'lodash-es'
 import { useNow } from '@vueuse/core'
 import type { ActionType, BotKind, GoogleSheetsRow } from '@/services/googleSheetsService'
 
